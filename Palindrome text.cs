accb0bc56d56e5dda096d14e8c2cf0ab3f65f0dc
Nikolas <34384886+Dudiak-Nikolas@users.noreply.github.com>
2020-09-10 03:08:13 +0300

    Add files via upload

---
+++ Palindrome text.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Palindrome_text
+{
+    class Solution 
+    {
+        public static bool IsPalindrome(string x)
+        {
+            if (x.Length <= 1)
+                return true;
+            else
+            {
+                if (x[0] != x[x.Length - 1])
+                    return false;
+                else
+                    return IsPalindrome(x.Substring(1, x.Length - 2));
+            }
+        }
+        static void Main(string[] args)
+        {
+            string vin, Text, Lov, Tow;
+            bool to;
+            
+
+            Console.Write("\n Для того чтобы проверить есть ли Текст палиндромным укажите снизу эго:\n");
+            Console.Write("---------------------------------------------------------------\n ");
+
+            Console.Write(" Укажите Текст : ");
+            vin = Console.ReadLine();
+            Lov = vin.ToLower();
+            Regex myReg = new Regex(@"[a-zA-Z,\s]{5,50}");
+            Tow = Regex.Replace(Lov, @"\d", " ");
+            Match match = myReg.Match(Tow);
+
+            Text = match.Value;
+           
+            to = IsPalindrome(Text);
+
+            if(to == true)
+            {
+                Console.WriteLine(" Этот текст являются палиндромним.\n");
+            }
+            else
+            {
+                Console.WriteLine(" Этот текст не являются палиндромним.\n");
+            }
+            Console.Read();
+        }
+    }
+}
